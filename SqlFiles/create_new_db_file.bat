2926e26fc1aade18af5dc43a9c6ec742803bf5fc
Morten Fjeldstad <morten.fjeldstad@gmail.com>
2007-12-31 13:48:50 +0000

    Fixed typo in create script.

--- SqlFiles/create_new_db_file.bat
+++ SqlFiles/create_new_db_file.bat
@@ -5,5 +5,5 @@ echo Creating aoitems.db tables and views.
 echo Importing item list.
 ..\Externals\SQLite\sqlite3.exe -init aoitems_import.sql aoitems.db .quit
 
-echo Building pockectboss patters. This will take a while!
+echo Building pockectboss patterns. This will take a while!
 ..\Externals\SQLite\sqlite3.exe -init build_tPatterns.sql aoitems.db .quit
