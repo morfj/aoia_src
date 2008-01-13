e68fe2b1d51927d28c2b7e75edc36ff64d0fa1d6
Morten Fjeldstad <morten.fjeldstad@gmail.com>
2008-01-13 17:15:39 +0000

    - Added some proper header guards.

--- PluginSDK/ItemAssistView.h
+++ PluginSDK/ItemAssistView.h
@@ -1,47 +1,50 @@
-#pragma once
+#ifndef ITEMASSISTVIEW_H
+#define ITEMASSISTVIEW_H
 
 #include <PluginSDK/PluginViewInterface.h>
 
 
 template < class T >
 class ItemAssistView
-   : public CWindowImpl<T>
-   , public PluginViewInterface
+  : public CWindowImpl<T>
+  , public PluginViewInterface
 {
-   typedef ItemAssistView<T> ThisType;
+    typedef ItemAssistView<T> ThisType;
 public:
-	BEGIN_MSG_MAP_EX(ThisType)
-      MSG_WM_ERASEBKGND(OnEraseBkgnd)
-	END_MSG_MAP()
+    BEGIN_MSG_MAP_EX(ThisType)
+        MSG_WM_ERASEBKGND(OnEraseBkgnd)
+    END_MSG_MAP()
 
-   LRESULT OnEraseBkgnd(HDC dc) { return 1; }
+    LRESULT OnEraseBkgnd(HDC dc) { return 1; }
 
-   ItemAssistView(void)
-   {
-   }
+    ItemAssistView(void)
+    {
+    }
 
-   virtual ~ItemAssistView(void)
-   {
-   }
+    virtual ~ItemAssistView(void)
+    {
+    }
 
-   virtual bool PreTranslateMsg(MSG* pMsg)
-   {
-      // Override in derived classes
-      return false;
-   }
+    virtual bool PreTranslateMsg(MSG* pMsg)
+    {
+        // Override in derived classes
+        return false;
+    }
 
-   virtual void OnAOMessage(AO::Header *pMsg)
-   {
-      // Override in derived classes
-   }
+    virtual void OnAOMessage(AO::Header *pMsg)
+    {
+        // Override in derived classes
+    }
 
-   virtual HWND GetWindow() const
-   {
-      return m_hWnd;
-   }
+    virtual HWND GetWindow() const
+    {
+        return m_hWnd;
+    }
 
-   virtual void OnActive(bool doActivation)
-   {
-      // Override in derived classes
-   }
+    virtual void OnActive(bool doActivation)
+    {
+        // Override in derived classes
+    }
 };
+
+#endif // ITEMASSISTVIEW_H
--- PluginSDK/PluginViewInterface.h
+++ PluginSDK/PluginViewInterface.h
@@ -1,13 +1,14 @@
-
-#pragma once
+#ifndef PLUGINVIEWINTERFACE_H
+#define PLUGINVIEWINTERFACE_H
 
 #include "shared/aopackets.h"
 
-
 struct PluginViewInterface
 {
-	virtual void OnAOMessage(AO::Header *pMsg) = 0;
-   virtual bool PreTranslateMsg(MSG* pMsg) = 0;
-   virtual HWND GetWindow() const = 0;
-   virtual void OnActive(bool doActivation) = 0;
+    virtual void OnAOMessage(AO::Header *pMsg) = 0;
+    virtual bool PreTranslateMsg(MSG* pMsg) = 0;
+    virtual HWND GetWindow() const = 0;
+    virtual void OnActive(bool doActivation) = 0;
 };
+
+#endif // PLUGINVIEWINTERFACE_H
