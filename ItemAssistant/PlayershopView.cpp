441672c6aab7be4b11e33e5448264caaa47c4d2b
Kenneth Ellefsen <kenneth.ellefsen@gmail.com>
2008-08-10 11:33:35 +0000

    We have PlayerShopPopups! :)

--- ItemAssistant/PlayershopView.cpp
+++ ItemAssistant/PlayershopView.cpp
@@ -3,7 +3,12 @@
 //#include <boost/filesystem/path.hpp>
 //#include <boost/filesystem/fstream.hpp>
 //#include <boost/filesystem/operations.hpp>
-
+//#include <boost/algorithm/string.hpp>
+//#include <boost/algorithm/string/find_iterator.hpp>
+//#include <boost/algorithm/string/find_format.hpp>
+//#include <boost/algorithm/string/replace.hpp>
+//#include <boost/algorithm/string/erase.hpp>
+#include <boost/algorithm/string/find.hpp>
 
 enum ColumnID
 {
@@ -15,10 +20,13 @@ enum ColumnID
 PlayershopView::PlayershopView(void)
 : m_sortColumn(0)
 {
+   m_directoryWatch.Begin();
 }
 
 PlayershopView::~PlayershopView(void)
 {
+   m_directoryWatch.StopPlease();
+   m_directoryWatch.End();
 }
 
 
@@ -178,4 +186,275 @@ void PlayershopView::OnActive(bool doActivation)
    }
 
 
+}
+
+
+void WatchDirectoryThread::WatchDirectory(LPTSTR lpDir)
+{
+   //DWORD dwWaitStatus; 
+/*   HANDLE dwChangeHandles[2]; 
+   TCHAR lpDrive[4];
+   TCHAR lpFile[_MAX_FNAME];
+   TCHAR lpExt[_MAX_EXT];
+
+   _tsplitpath_s(lpDir, lpDrive, 4, NULL, 0, lpFile, _MAX_FNAME, lpExt, _MAX_EXT);
+
+   lpDrive[2] = (TCHAR)'\\';
+   lpDrive[3] = (TCHAR)'\0';
+ 
+// Watch the directory for file creation and deletion.  
+   dwChangeHandles[0] = FindFirstChangeNotification( 
+      lpDir,                          // directory to watch 
+      TRUE,                          // watch the subtree 
+      FILE_NOTIFY_CHANGE_LAST_WRITE |
+	  FILE_NOTIFY_CHANGE_CREATION);  // watch file write or creation
+ */
+HANDLE directory = CreateFile(
+      lpDir,                          // directory to watch 
+      GENERIC_READ,
+      FILE_SHARE_READ|FILE_SHARE_WRITE,
+      NULL,
+      OPEN_EXISTING,
+      FILE_FLAG_BACKUP_SEMANTICS ,
+      NULL
+);
+
+char buffer[4096];
+/*
+
+BOOL WINAPI ReadDirectoryChangesW(
+  directory,
+  buffer,
+  4096,
+  TRUE,
+  FILE_NOTIFY_CHANGE_LAST_WRITE|FILE_NOTIFY_CHANGE_CREATION
+);
+*/
+/*
+   if (dwChangeHandles[0] == INVALID_HANDLE_VALUE) 
+   {
+     printf("\n ERROR: FindFirstChangeNotification function failed.\n");
+     ExitProcess(GetLastError()); 
+   }
+ */
+// Watch the subtree for directory creation and deletion.  
+/*   dwChangeHandles[1] = FindFirstChangeNotification( 
+      lpDrive,                       // directory to watch 
+      TRUE,                          // watch the subtree 
+      FILE_NOTIFY_CHANGE_DIR_NAME);  // watch dir name changes 
+ 
+   if (dwChangeHandles[1] == INVALID_HANDLE_VALUE) 
+   {
+     printf("\n ERROR: FindFirstChangeNotification function failed.\n");
+     ExitProcess(GetLastError()); 
+   }
+ */
+
+// Make a final validation check on our handles.
+/*   if ((dwChangeHandles[0] == NULL))// || (dwChangeHandles[1] == NULL))
+   {
+     printf("\n ERROR: Unexpected NULL from FindFirstChangeNotification.\n");
+     ExitProcess(GetLastError()); 
+   }
+*/
+// Change notification is set. Now wait on both notification 
+// handles and refresh accordingly.  
+   while (TRUE) 
+   { 
+      // Wait for notification. 
+      printf("\nWaiting for notification...\n");
+      DWORD bytesReturned = 0;
+
+      ReadDirectoryChangesW(
+         directory,
+         buffer,
+         4096,
+         TRUE,
+         FILE_NOTIFY_CHANGE_LAST_WRITE|FILE_NOTIFY_CHANGE_CREATION,
+         &bytesReturned,
+         NULL,
+         NULL
+      );
+
+//      RefreshTree(NULL); 
+
+      std::tstringstream f;
+      f << lpDir << "\\";
+      for(int i=12;i<bytesReturned;i=i+2)
+      {
+      f << buffer[i];
+      }
+
+   std::tstring filename(f.str());
+
+    //char text[]="hello dolly!";
+   //iterator_range<char*> result=find_last(filename.c_str(),"PlayerShopLog.html");
+
+    //transform( result.begin(), result.end(), result.begin(), bind2nd(plus<char>(), 1) );
+    // text = "hello dommy!"            
+
+    //to_upper(result); // text == "hello doMMy!"
+
+    // iterator_range is convertible to bool
+   const wchar_t* ptr = filename.c_str();
+   if(!boost::find_last(ptr, "PlayerShopLog.html"))
+    {
+        return;
+    }
+
+   boost::filesystem::path p(to_utf8_copy(filename),boost::filesystem::native);
+
+   std::ifstream in(p.string().c_str());
+   std::string line;
+   std::string text;
+   std::vector<std::tstring> v;
+   while(in){
+      line.clear();
+      std::getline(in,line);
+      if(!line.empty())
+      {
+         text += line;
+      }
+   }
+
+   // Now that we have the whole file, lets parse it
+
+   // Text located between the two following tags is to be considered an item sold
+   std::string startTag = "<div indent=wrapped>" ;
+   std::string endTag   = "</div>" ;
+   std::stringstream popupText;
+   while(text.length() > 0)
+   {
+   
+      std::string::size_type start = text.find( startTag, 0 );
+      std::string::size_type end  = text.find( endTag , 0 );
+      if( start != std::string::npos && end != std::string::npos)
+      {
+         // adding data for column 1
+         //v.push_back(from_ascii_copy(text.substr(start+startTag.length(),end-start-startTag.length())));
+         popupText << text.substr(start+startTag.length(),end-start-startTag.length()) << "\r\n";
+
+         // remove the already processed part of the string
+         text = text.substr(end+endTag.length());
+      }
+      else
+      {
+         text = "";
+      }
+   }
+
+/*
+   if(!m_popup)
+   {
+      m_popup = new CPlayerShopPopupDlg;
+   }
+   
+   if(!::IsWindow(m_popup->GetSafeHwnd()))
+   {
+      m_popup->Create(IDD_PLAYERSHOPPOPUP,this);
+   }
+*/
+   ServicesSingleton::Instance()->ShowTrayIconBalloon(STREAM2STR(popupText.str().c_str()));
+//   popup.Create(NULL);
+/*
+   PlayerShopPopupDlg popup;
+   popup.Create(NULL);
+   popup.ShowWindow(SW_SHOW);
+   popup->SetText(popupText.str());
+*/
+   //popup.ShowWindow(0);
+   //popup.DoModal();
+//   m_popup->SetText(popupText.str());
+//   m_popup->ShowWindow(SW_SHOW);
+
+   //Sleep(5000);
+   //popup.ShowWindow(SW_HIDE);
+   //popup.DestroyWindow();
+
+/*
+
+      dwWaitStatus = WaitForMultipleObjects(1, dwChangeHandles, 
+         FALSE, INFINITE); 
+ 
+      switch (dwWaitStatus) 
+      { 
+         case WAIT_OBJECT_0: 
+ 
+         // A file was created or written.
+         // Refresh tree
+             RefreshTree(NULL); 
+             if ( FindNextChangeNotification(dwChangeHandles[0]) == FALSE )
+             {
+               printf("\n ERROR: FindNextChangeNotification function failed.\n");
+               ExitProcess(GetLastError()); 
+             }
+             break; 
+ 
+         case WAIT_OBJECT_0 + 1: 
+ 
+         // A directory was created, renamed, or deleted.
+         // Refresh the tree and restart the notification. 
+             RefreshTree(lpDrive); 
+             if (FindNextChangeNotification(dwChangeHandles[1]) == FALSE )
+             {
+               printf("\n ERROR: FindNextChangeNotification function failed.\n");
+               ExitProcess(GetLastError()); 
+             }
+             break; 
+ 
+         case WAIT_TIMEOUT:
+
+         // A time-out occurred. This would happen if some value other 
+         // than INFINITE is used in the Wait call and no changes occur.
+         // In a single-threaded environment, you might not want an
+         // INFINITE wait.
+ 
+            printf("\nNo changes in the time-out period.\n");
+            break;
+
+         default: 
+            printf("\n ERROR: Unhandled dwWaitStatus.\n");
+            ExitProcess(GetLastError());
+            break;
+      }*/
+   }
+}
+
+void WatchDirectoryThread::RefreshDirectory(LPTSTR lpDir)
+{
+   // This is where you might place code to refresh your
+   // directory listing, but not the subtree because it
+   // would not be necessary.
+
+   _tprintf(TEXT("Directory (%s) changed.\n"), lpDir);
+}
+
+void WatchDirectoryThread::RefreshTree(LPTSTR lpDrive)
+{
+   // This is where you might place code to refresh your
+   // directory listing, including the subtree.
+
+   _tprintf(TEXT("Directory tree (%s) changed.\n"), lpDrive);
+}
+
+/*************************************************/
+/** Worker Thread                               **/
+/*************************************************/
+
+DWORD WatchDirectoryThread::ThreadProc()
+{
+   m_term = false;
+   std::tstring filename;
+   filename = STREAM2STR( g_DBManager.AOFolder() << _T("\\Prefs") );
+
+   do
+   {
+      WatchDirectory((LPTSTR)filename.c_str());
+//      WatchDirectory(dummy.c_str());
+   } 
+   while (!m_term);
+
+   m_term = false;
+
+   return 0;
 }
--- ItemAssistant/PlayershopView.h
+++ ItemAssistant/PlayershopView.h
@@ -1,73 +1,100 @@
 #pragma once
 
 #include <PluginSDK/ItemAssistView.h>
+#include <boost/filesystem/path.hpp>
+#include "shared/Thread.h"
+#include "shared/Mutex.h"
 #include <atlsplit.h>
 #include "MFTreeView.h"
 #include "PsmTreeItems.h"
 
+class WatchDirectoryThread : public Thread
+{
+public:
+	WatchDirectoryThread(): m_term(false)  { }
+   virtual ~WatchDirectoryThread() { }
+
+
+	void RefreshTree(LPTSTR lpDrive);
+	void RefreshDirectory(LPTSTR lpDir);
+	void WatchDirectory(LPTSTR lpDir);
+
+   void SetOwner(PlayershopView* owner) { m_pOwner = owner; }
+   void StopPlease() { if (IsRunning()) { m_term = true; } }
+
+   virtual DWORD ThreadProc();
+
+private:
+   PlayershopView* m_pOwner;
+   bool m_term;
+
+};
 class PlayershopView :
     public ItemAssistView<PlayershopView>
 {
     typedef ItemAssistView<PlayershopView> inherited;
 public:
-    //enum { IDD = IDD_PLAYERSHOP };
-    DECLARE_WND_CLASS(NULL)
+   //enum { IDD = IDD_PLAYERSHOP };
+   DECLARE_WND_CLASS(NULL)
 
-    PlayershopView(void);
-    virtual ~PlayershopView(void);
+   PlayershopView(void);
+   virtual ~PlayershopView(void);
 
-    enum
-    {
-        WM_POSTCREATE = WM_APP + 1,
-    };
+   enum
+   {
+      WM_POSTCREATE = WM_APP + 1,
+   };
 
-    enum {
-        IDW_PSMLISTVIEW = 1,
-        IDW_PSMTREEVIEW
-    };
+   enum {
+      IDW_PSMLISTVIEW = 1,
+      IDW_PSMTREEVIEW
+   };
 
-    BEGIN_MSG_MAP_EX(PlayershopView)
-        MSG_WM_CREATE(OnCreate)
-        MSG_WM_SIZE(OnSize)
-        MESSAGE_HANDLER(WM_POSTCREATE, OnPostCreate)
-/*      COMMAND_ID_HANDLER(ID_INV_FIND, OnFind)
-        COMMAND_ID_HANDLER(ID_INV_FIND_HIDE, OnFindHide)
-        COMMAND_ID_HANDLER(ID_INFO, OnInfo)
-        NOTIFY_CODE_HANDLER_EX(LVN_COLUMNCLICK, OnColumnClick)
-        NOTIFY_CODE_HANDLER_EX(LVN_ITEMACTIVATE, OnItemActivate)
-        CHAIN_MSG_MAP(inherited)*/
-        REFLECT_NOTIFICATIONS()
-        DEFAULT_REFLECTION_HANDLER()
-    END_MSG_MAP()
+   BEGIN_MSG_MAP_EX(PlayershopView)
+      MSG_WM_CREATE(OnCreate)
+      MSG_WM_SIZE(OnSize)
+      MESSAGE_HANDLER(WM_POSTCREATE, OnPostCreate)
+      /*      COMMAND_ID_HANDLER(ID_INV_FIND, OnFind)
+      COMMAND_ID_HANDLER(ID_INV_FIND_HIDE, OnFindHide)
+      COMMAND_ID_HANDLER(ID_INFO, OnInfo)
+      NOTIFY_CODE_HANDLER_EX(LVN_COLUMNCLICK, OnColumnClick)
+      NOTIFY_CODE_HANDLER_EX(LVN_ITEMACTIVATE, OnItemActivate)
+      CHAIN_MSG_MAP(inherited)*/
+      REFLECT_NOTIFICATIONS()
+      DEFAULT_REFLECTION_HANDLER()
+   END_MSG_MAP()
 
-    LRESULT OnCreate(LPCREATESTRUCT createStruct);
-    LRESULT OnSize(UINT wParam, CSize newSize);
-    LRESULT OnPostCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
-    LRESULT OnColumnClick(LPNMHDR lParam);
-    LRESULT OnItemActivate(LPNMHDR lParam);
+   LRESULT OnCreate(LPCREATESTRUCT createStruct);
+   LRESULT OnSize(UINT wParam, CSize newSize);
+   LRESULT OnPostCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
+   LRESULT OnColumnClick(LPNMHDR lParam);
+   LRESULT OnItemActivate(LPNMHDR lParam);
 
-    void UpdateListView(std::tstring const& where);
-    void UpdateListView(std::vector<std::tstring> v);
+   void UpdateListView(std::tstring const& where);
+   void UpdateListView(std::vector<std::tstring> v);
 
-    // ItemAssistView overrides
-    virtual void OnActive(bool doActivation);
+   // ItemAssistView overrides
+   virtual void OnActive(bool doActivation);
 
 protected:
-    void UpdateLayout(CSize newSize);
-    static int CALLBACK CompareStr(LPARAM param1, LPARAM param2, LPARAM sort);
+   void UpdateLayout(CSize newSize);
+   static int CALLBACK CompareStr(LPARAM param1, LPARAM param2, LPARAM sort);
 
 private:
-    CSplitterWindow   m_splitter;
 
-    //MFTreeView        m_treeview;
-    PsmTreeView		   m_treeview;
-    CListViewCtrl     m_listview;
-    //CTreeItem         m_treeRoot;
 
-    PlayershopTreeRoot m_treeRoot;
+   CSplitterWindow   m_splitter;
 
-    bool  m_sortDesc;
-    int   m_sortColumn;
+   //MFTreeView        m_treeview;
+   PsmTreeView		   m_treeview;
+   CListViewCtrl     m_listview;
+   //CTreeItem         m_treeRoot;
 
+   PlayershopTreeRoot m_treeRoot;
+   WatchDirectoryThread m_directoryWatch;
+
+   bool  m_sortDesc;
+   int   m_sortColumn;
 
 };
+
