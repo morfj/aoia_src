05c7aefc720ad549097ee4f4d51176f00d293b87
Morten Fjeldstad <morten.fjeldstad@gmail.com>
2008-11-17 20:57:47 +0000

    Added some missing files in the PluginSubSystem.

---
+++ PluginSubSystem/stdafx.h
@@ -0,0 +1,9 @@
+#ifndef STDAFX_H
+#define STDAFX_H
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+//#include <atlbase.h>
+
+#endif // STDAFX_H
