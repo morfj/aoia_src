5c65807c225ccd498deec573aa3d357e5318af2e
Morten Fjeldstad <morten.fjeldstad@gmail.com>
2011-12-07 11:37:31 +0100

    Added more logging, and made the function return false in all error conditions.

--- ItemAssistant/InjectionSupport.cpp
+++ ItemAssistant/InjectionSupport.cpp
@@ -3,36 +3,44 @@
 
 bool InjectDLL(DWORD ProcessID, std::tstring const& dllName)
 {
-    Logger::instance().log(STREAM2STR("Attempting to inject " << dllName));
+    if (!ProcessID)
+    {
+        LOG("No process ID specified.");
+        return false;
+    }
+    LOG("Attempting to inject '" << dllName << "' into process " << ProcessID);
 
-    std::string dllNameA = to_ascii_copy(dllName);
-
-    if(!ProcessID) {
-        Logger::instance().log(_T("No process ID specified to inject into."));
+    HANDLE Proc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessID);
+    if (!Proc)
+    {
+        LOG("Could not open process. Error code: " << GetLastError());
         return false;
     }
 
-    HANDLE Proc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessID);
+    std::string dllNameA = to_ascii_copy(dllName);
+    LPVOID RemoteString = (LPVOID)VirtualAllocEx(Proc, NULL, dllNameA.length(), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+    if (!RemoteString)
+    {
+        LOG("Could not allocate memory in remote process. Error code: " << GetLastError());
+        CloseHandle(Proc);
+        return false;
+    }
 
-    if(!Proc) {
-        Logger::instance().log(STREAM2STR("Could not open process. Error code: " << GetLastError()));
+    if (!WriteProcessMemory(Proc, (LPVOID)RemoteString, dllNameA.c_str(), dllNameA.length(), NULL))
+    {
+        LOG("Failed to send DLL name to AO client. Error: " << GetLastError());
+        CloseHandle(Proc);
         return false;
     }
 
     LPVOID LoadLibAddy = (LPVOID)GetProcAddress(GetModuleHandle(_T("kernel32.dll")), "LoadLibraryA");
-    LPVOID RemoteString = (LPVOID)VirtualAllocEx(Proc, NULL, dllNameA.length(), MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
-    if (!RemoteString) {
-        Logger::instance().log(STREAM2STR("Could not allocate memory in remote process. Error code: " << GetLastError()));
+    if (!CreateRemoteThread(Proc, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibAddy, (LPVOID)RemoteString, NULL, NULL))
+    {
+        LOG("Injection failed with error code: " << GetLastError());
+        CloseHandle(Proc);
         return false;
-    }
-    WriteProcessMemory(Proc, (LPVOID)RemoteString, dllNameA.c_str(), dllNameA.length(), NULL);
-
-    if(!CreateRemoteThread(Proc, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibAddy, (LPVOID)RemoteString, NULL, NULL)) {
-        DWORD error = GetLastError();
-        Logger::instance().log(STREAM2STR("Injection failed with error code: " << error));
     }
 
     CloseHandle(Proc);
-
     return true;
-} 
+}
